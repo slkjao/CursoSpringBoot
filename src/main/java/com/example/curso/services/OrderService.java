8e505816721610071a90e9078ebac1f62a687563
joao costa <joaovaraujoc@gmail.com>
2022-05-03 15:30:10 -0300

    Order

---
+++ src/main/java/com/example/curso/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.example.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.curso.entities.Order;
+import com.example.curso.entities.User;
+import com.example.curso.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> opt =  orderRepository.findById(id);
+		return opt.get();
+	}
+}
